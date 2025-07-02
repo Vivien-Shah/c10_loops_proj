d01f7a0e703ca85359760e0fdd497f0ce0f4f410
Vivien-Shah <hfsthane7787@gmail.com>
2025-07-02 19:47:54 +0530

    Add files via upload

---
+++ c10_loops_proj.py
@@ -0,0 +1,9 @@
+'''Power Calculator
+num=int(input("Enter your number: "))
+n=int(input("Enter the number's power: "))
+ans=1
+for i in range (n):
+    ans=ans*num
+print("The answer is :",ans)'''
+
+
